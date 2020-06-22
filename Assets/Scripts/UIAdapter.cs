8412c5b12eafbbb93a6efa27bce2ae0cd964016e
XiaoyaWan  <yaxuanou0428@gmail.com>
2020-06-22 01:12:53 -0700

    first commit

---
+++ Assets/Scripts/UIAdapter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAdapter : MonoBehaviour
+{
+    public GameObject scorePanel;
+    public GameObject pausePanel;
+    public GameObject resumeButton;
+    public GameObject settingPanel;    
+    public GameObject settingButton;
+    public GameObject failPanel;
+    public GameObject leftButton;
+    public GameObject rightButton;
+    public GameObject perfect;
+    public GameObject canvas;
+
+    void Update()
+    {
+        if (Screen.orientation == ScreenOrientation.Portrait)
+        {
+            scorePanel.transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+            scorePanel.transform.localPosition = new Vector3(80, 130, 0);
+            pausePanel.transform.localScale = new Vector3(1, 1, 1);
+            pausePanel.transform.localPosition = new Vector3(100, 180, 0);
+            resumeButton.transform.localScale = new Vector3(1, 1, 1);
+            resumeButton.transform.localPosition = new Vector3(100, 180, 0);
+            settingButton.transform.localScale = new Vector3(1, 1, 1);
+            settingButton.transform.localPosition = new Vector3(100, 220, 0);
+            leftButton.transform.localScale = new Vector3(1, 1, 1);
+            leftButton.transform.localPosition = new Vector3(-60, -190, 0);
+            rightButton.transform.localScale = new Vector3(1, 1, 1);
+            rightButton.transform.localPosition = new Vector3(60, -190, 0);
+            settingPanel.transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+            failPanel.transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+            perfect.transform.localScale = new Vector3(2, 1, 1);
+            canvas.GetComponent<CanvasScaler>().screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+
+        }
+        if (Screen.orientation == ScreenOrientation.Landscape)
+        {
+            scorePanel.transform.localScale = new Vector3(1, 1, 1);
+            scorePanel.transform.localPosition = new Vector3(0, 0, 0);
+            pausePanel.transform.localScale = new Vector3(2, 2, 1);
+            pausePanel.transform.localPosition = new Vector3(280, 200, 0);
+            resumeButton.transform.localScale = new Vector3(2, 2, 1);
+            resumeButton.transform.localPosition = new Vector3(280, 200, 0);
+            settingButton.transform.localScale = new Vector3(2, 2, 1);
+            settingButton.transform.localPosition = new Vector3(370, 200, 0);
+            leftButton.transform.localScale = new Vector3(1.5f, 1.5f, 1f);
+            leftButton.transform.localPosition = new Vector3(-300, -190, 0);
+            rightButton.transform.localScale = new Vector3(1.5f, 1.5f, 1f);
+            rightButton.transform.localPosition = new Vector3(300, -190, 0);
+            settingPanel.transform.localScale = new Vector3(1, 1, 1);
+            failPanel.transform.localScale = new Vector3(1, 1, 1);
+            perfect.transform.localScale = new Vector3(4, 2, 1);
+            canvas.GetComponent<CanvasScaler>().screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
+        }
+    }
+}
---
+++ Assets/Scripts/UIAdapterForMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAdapterForMenu : MonoBehaviour
+{
+    public GameObject title;
+    public GameObject playButton;
+    public GameObject quitButton;
+    public Image bg;
+    public GameObject canvas;
+
+    void Update()
+    {
+        if (Screen.orientation == ScreenOrientation.Portrait || Screen.orientation == ScreenOrientation.PortraitUpsideDown)
+        {
+            title.transform.localScale = new Vector3(3f, 3f, 1f);
+            title.transform.localPosition = new Vector3(10, 150, 0);
+            playButton.transform.localScale = new Vector3(1.5f, 0.5f, 1f);
+            playButton.transform.localPosition = new Vector3(0, -50, 0);
+            quitButton.transform.localScale = new Vector3(1.5f, 0.5f, 1f);
+            quitButton.transform.localPosition = new Vector3(0, -120, 0);
+            bg.sprite = Resources.Load<Sprite>("Sprites/background_ver");
+            canvas.GetComponent<CanvasScaler>().screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+        }
+        if (Screen.orientation == ScreenOrientation.LandscapeLeft || Screen.orientation == ScreenOrientation.LandscapeRight)
+        {
+            title.transform.localScale = new Vector3(5, 5, 1);
+            title.transform.localPosition = new Vector3(-120, 50, 0);
+            playButton.transform.localScale = new Vector3(3, 1, 1);
+            playButton.transform.localPosition = new Vector3(260, 0, 0);
+            quitButton.transform.localScale = new Vector3(3, 1, 1);
+            quitButton.transform.localPosition = new Vector3(260, -100, 0);
+            bg.sprite = Resources.Load<Sprite>("Sprites/background_hor");
+            canvas.GetComponent<CanvasScaler>().screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
+        }
+    }
+}
